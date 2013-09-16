7fe167bd67b66881b900686b1bb2dc76f4c565e4
frebro <frebro@gmail.com>
2013-09-16 09:27:30 +0200

    initialized from boilerplate

---
+++ views/admin.php
@@ -0,0 +1,6 @@
+<?php if ( is_plugin_active('event-organiser/event-organiser.php') ) :
+  echo 'yes';
+else:
+  echo 'This widget requires that you activate the <a href="http://wp-event-organiser.com/" target="_blank">Event Organiser plugin</a>.';
+endif;
+?>
