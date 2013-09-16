c811a73693ac23ada91f2092a461975e357e89b4
frebro <frebro@gmail.com>
2013-09-16 09:52:08 +0200

    added event listing to admin

--- views/admin.php
+++ views/admin.php
@@ -1,5 +1,23 @@
 <?php if ( is_plugin_active('event-organiser/event-organiser.php') ) :
-  echo 'yes';
+  $events = eo_get_events();
+
+  if($events):
+    echo '<label for="eosw-eventid">Associate sponsors to event</label>';
+    echo '<select id="eosw-eventid">';
+    echo '<option value="-1">All</option>';
+    foreach ($events as $event):
+
+      //Check if all day, set format accordingly
+      $format = ( eo_is_all_day($event->ID) ? get_option('date_format') : get_option('date_format').' '.get_option('time_format') );
+
+      printf(
+             '<option value="%s">%s</option>',
+             $event->ID,
+             get_the_title($event->ID)
+          );
+    endforeach;
+    echo '</select>';
+  endif;
 else:
   echo 'This widget requires that you activate the <a href="http://wp-event-organiser.com/" target="_blank">Event Organiser plugin</a>.';
 endif;
